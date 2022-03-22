b6ed8a7ad8aa791791df8072bfeceacbaec9acf4
yigong <yigong@upchina.com>
2022-03-22 14:26:14 +0800

    'init'

---
+++ src/libs/LoveMsg/index.ts
@@ -0,0 +1,27 @@
+/**
+ * @name loveMsg
+ * @description 入口
+ */
+import dotenv from 'dotenv'
+import { goodMorning } from './goodMorning'
+import { goodAfternoon } from './goodAfternoon'
+import { goodEvening } from './goodEvening'
+dotenv.config()
+
+const { MESSAGE_TYPE } = process.env
+console.log('MESSAGE_TYPE:'+MESSAGE_TYPE)
+
+export default function main() {
+  if (MESSAGE_TYPE === 'goodAfternoon') {
+    // 午安
+    goodAfternoon()
+  }
+  else if (MESSAGE_TYPE === 'goodEvening') {
+    // 晚安
+    goodEvening()
+  }
+  else {
+    // 早安
+    goodMorning()
+  }
+}
---
+++ src/libs/LoveMsg/templates/text.ts
@@ -0,0 +1,69 @@
+/**
+ * @description 纯文本模板-企业微信消息通知
+ * https://open.work.weixin.qq.com/api/doc/90000/90135/90236
+ */
+
+import dayjs, { weekToday } from '../../../utils/dayjs'
+
+export const textTemplate = (data: TextTemplateProps) => {
+  const { caiHongpi, sayLove, songLyrics, oneMagazines, netEaseCloud, oneWord, dayEnglish } = data
+
+  let text = '早安😆ljfwxy\n'
+
+  // 工作日/休息日，需要排除节假日
+  const week = weekToday()
+  if (['星期六', '星期日'].includes(week)) {
+    text += `今天可是${week}哦，继续睡懒觉~`
+  }
+  else {
+    text += `今天是${week}，赶紧起来上班~😝`
+  }
+
+  // 添加笑话
+  if (caiHongpi) {
+    //     text += `
+    // 彩虹屁：
+    text += `
+${caiHongpi.content}\n`
+  }
+
+  if (sayLove) {
+    text += `
+${sayLove.content}\n`
+  }
+
+  // 诗句
+  if (songLyrics) {
+    text += `
+『${songLyrics.source}』${songLyrics.content}\n`
+  }
+
+  if (oneMagazines) {
+    text += `
+『ONE杂志』${oneMagazines.word}\n`
+  }
+
+  if (netEaseCloud) {
+    text += `
+『网易云音乐热评』${netEaseCloud.content}——${netEaseCloud.source}\n`
+  }
+
+  // 添加一句一言
+  if (oneWord) {
+    text += `
+『一言』${oneWord.hitokoto}\n`
+  }
+
+  // 每日英语
+  if (dayEnglish) {
+    text += `
+『每日英语（${dayjs(dayEnglish.date).format('ll')}』${dayEnglish.content}`
+  }
+
+  return {
+    msgtype: 'text',
+    text: {
+      content: text,
+    },
+  }
+}
